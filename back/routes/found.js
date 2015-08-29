297e2718aa1e5827130fe2ebc61be40c10a55ae8
Chris McKenzie <kristopolous@yahoo.com>
2015-08-29 15:18:52 -0700

    moving things up

--- back/routes/found.js
+++ back/routes/found.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // announce you've found something.
 router.post('/', function(req, res, next) {
-  console.log(req,res,next);
+  console.log(req.body);
   res.render('index', { title: 'Express' });
 });
 
