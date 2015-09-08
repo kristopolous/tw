5df1e0bec323d063288f0bcaedb6cd888e2de9a6
Chris McKenzie <kristopolous@yahoo.com>
2015-09-08 15:36:50 -0700

    injecting title

--- back/routes/index.js
+++ back/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('base', { page: 'index' });
+  res.render('base', { page: 'index', opts: {title: 'title'} });
 });
 
 router.get('/pro', function(req, res, next) {
