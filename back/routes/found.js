32813aebba65276d855ea7bd4cb8b2ac5cb88fd4
Chris McKenzie <kristopolous@yahoo.com>
2015-09-13 17:59:54 -0700

    reworking the found api

--- back/routes/found.js
+++ back/routes/found.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+  res.render('base', { page: 'post', title: 'title'} );
+});
+
 // announce you've found something.
 router.post('/', function(req, res, next) {
   console.log(req.body);
