535630bf4284ab7d12dc588dcf273a45dab892f4
Chris McKenzie <kristopolous@yahoo.com>
2015-09-14 22:04:22 -0700

    better naming

--- back/routes/book.js
+++ back/routes/book.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-  res.render('base', { page: 'book', title: 'title'} );
+  res.render('base', { page: 'book', title: 'Book a service'} );
 });
 
 module.exports = router;
--- back/routes/find.js
+++ back/routes/find.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { page: 'find', title: 'Express' });
 });
 
 module.exports = router;
