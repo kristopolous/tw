7a0826e4ce1c076c63faa671a67fcb6e1b22d97d
Chris McKenzie <kristopolous@yahoo.com>
2015-09-14 22:53:39 -0700

    more landing page work

--- back/routes/index.js
+++ back/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('base', { page: 'index', title: 'title'} );
+  res.render('base', { page: 'index', title: 'Introducing two words'} );
 });
 
 router.get('/pro', function(req, res, next) {
