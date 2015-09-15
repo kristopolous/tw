4e72b5c0ffef8344637b0bf07791b3647ad916f5
Chris McKenzie <kristopolous@yahoo.com>
2015-09-14 22:11:14 -0700

    doing the find ep

--- back/routes/find.js
+++ back/routes/find.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.post('/', function(req, res, next) {
-  res.render('index', { page: 'find', title: 'Express' });
+router.get('/', function(req, res, next) {
+  res.render('base', { page: 'find', title: 'Find something' });
 });
 
 module.exports = router;
