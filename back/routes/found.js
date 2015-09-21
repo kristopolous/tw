c2842ec34d277b7a58f4e421a4e5920e2b1cdd40
Chris McKenzie <kristopolous@yahoo.com>
2015-09-20 17:34:16 -0700

    fixing the found and find

--- back/routes/found.js
+++ back/routes/found.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('base', { page: 'post', title: 'title'} );
+  res.render('base', { page: 'post', title: 'found something', type: 'user' } );
 });
 
 // announce you've found something.
